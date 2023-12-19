c13d68e325cf4779621d9c750bdb8877b3c87e83
SATRIYO WITJAKSONO <satriyowitjaksono2205@gmail.com>
2023-12-19 14:03:26 +0700

    himsi final modal

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { lazy, Suspense } from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 const Gallery = lazy(async () => import('pages/Gallery'))
-const Details = lazy(async () => import('pages/Details'))
 
 export default function App(): ReactElement {
 	return (
@@ -12,7 +11,6 @@ export default function App(): ReactElement {
 			<Suspense fallback={<LoadingOrError />}>
 				<Routes>
 					<Route path='/' element={<Gallery />} />
-					<Route path=':fruitName' element={<Details />} />
 				</Routes>
 			</Suspense>
 		</BrowserRouter>
